bfa4b2457402b8f73ac6c28e7541817d28d03d60
meredith <meredith@sentry.io>
2021-05-14 14:14:40 -0700

    try this

---
+++ script.py
@@ -0,0 +1,18 @@
+import os
+import sentry_sdk
+
+version = os.environ.get('$GITHUB_SHA', '1.0.0')
+
+sentry_sdk.init(dsn='https://39529b924487415380f43acf9a7d2335@o49697.ingest.sentry.io/5765882', release=version)
+
+def try_oncemore(text):
+  # not a thing
+  total = os.environ
+  version = os.environ.get('$GITHUB_SHA')
+  text.enlarge()
+  
+
+try: 
+   try_oncemore('meep')
+except Exception as err:
+   sentry_sdk.capture_exception(err)
