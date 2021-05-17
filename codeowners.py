5afeeff375a96274a7a339fef9a6f4853a397c8b
meredith <meredith@sentry.io>
2021-05-17 14:57:46 -0700

    try

---
+++ codeowners.py
@@ -0,0 +1,36 @@
+import os
+import requests
+
+
+
+# requests.post(url, data)
+
+URL = "https://sentry.io/api/0/organizations/meredith/soup/codeowners/"
+
+def get_file():
+    with open('./.github/CODEOWNERS', 'rt') as f:
+        print("hello")
+        data = f.read()
+    print(data)
+    # f.close()
+    return (data, f)
+
+def upload_file(file_data, f):
+    token = os.environ.get("SENTRY_AUTH_TOKEN")
+    code_mapping_id = os.environ.get("SENTRY_CODE_MAPPING_ID")
+    headers = {"Authentication": f"Bearer {token}"}
+    data = {
+        "codeMappingId": code_mapping_id,
+        "raw": file_data,
+    }
+    try:
+        resp = requests.post(URL, headers=headers, data=data)
+    except Exception as e:
+        print(f"something happened {str(e)}")
+
+    print(resp.status_code)
+    f.close()
+
+
+file_data, f = get_file()
+upload_file(file_data, f)
